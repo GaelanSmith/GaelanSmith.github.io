5737e6f1525cbb193fc2225919165b599c285d2c
Gaelan Smith <46384072+GaelanSmith@users.noreply.github.com>
2023-04-12 09:11:50 -0400

    Node data and styles

--- js/example1.js
+++ js/example1.js
@@ -29,12 +29,12 @@ var Log = {
 function init(){
     //init data
     var json = {
-        id: "node02",
-        name: "0.2",
+        id: "node08",
+        name: "You approach the border. How do you enter?",
         data: {},
         children: [{
-            id: "node13",
-            name: "1.3",
+            id: "node08.1",
+            name: "Go to a port of entry",
             data: {},
             children: [{
                 id: "node24",
@@ -143,8 +143,8 @@ function init(){
                 }]
             }]
         }, {
-            id: "node125",
-            name: "1.25",
+            id: "node08.2",
+            name: "Cross the border between ports of entry",
             data: {},
             children: [{
                 id: "node226",
@@ -342,421 +342,6 @@ function init(){
                     }]
                 }]
             }]
-        }, {
-            id: "node165",
-            name: "1.65",
-            data: {},
-            children: [{
-                id: "node266",
-                name: "2.66",
-                data: {},
-                children: [{
-                    id: "node367",
-                    name: "3.67",
-                    data: {},
-                    children: [{
-                        id: "node468",
-                        name: "4.68",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node469",
-                        name: "4.69",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node470",
-                        name: "4.70",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node471",
-                        name: "4.71",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node372",
-                    name: "3.72",
-                    data: {},
-                    children: [{
-                        id: "node473",
-                        name: "4.73",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node474",
-                        name: "4.74",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node475",
-                        name: "4.75",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node476",
-                        name: "4.76",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node377",
-                    name: "3.77",
-                    data: {},
-                    children: [{
-                        id: "node478",
-                        name: "4.78",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node479",
-                        name: "4.79",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node380",
-                    name: "3.80",
-                    data: {},
-                    children: [{
-                        id: "node481",
-                        name: "4.81",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node482",
-                        name: "4.82",
-                        data: {},
-                        children: []
-                    }]
-                }]
-            }, {
-                id: "node283",
-                name: "2.83",
-                data: {},
-                children: [{
-                    id: "node384",
-                    name: "3.84",
-                    data: {},
-                    children: [{
-                        id: "node485",
-                        name: "4.85",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node386",
-                    name: "3.86",
-                    data: {},
-                    children: [{
-                        id: "node487",
-                        name: "4.87",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node488",
-                        name: "4.88",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node489",
-                        name: "4.89",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node490",
-                        name: "4.90",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node491",
-                        name: "4.91",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node392",
-                    name: "3.92",
-                    data: {},
-                    children: [{
-                        id: "node493",
-                        name: "4.93",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node494",
-                        name: "4.94",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node495",
-                        name: "4.95",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node496",
-                        name: "4.96",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node397",
-                    name: "3.97",
-                    data: {},
-                    children: [{
-                        id: "node498",
-                        name: "4.98",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node399",
-                    name: "3.99",
-                    data: {},
-                    children: [{
-                        id: "node4100",
-                        name: "4.100",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4101",
-                        name: "4.101",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4102",
-                        name: "4.102",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4103",
-                        name: "4.103",
-                        data: {},
-                        children: []
-                    }]
-                }]
-            }, {
-                id: "node2104",
-                name: "2.104",
-                data: {},
-                children: [{
-                    id: "node3105",
-                    name: "3.105",
-                    data: {},
-                    children: [{
-                        id: "node4106",
-                        name: "4.106",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4107",
-                        name: "4.107",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4108",
-                        name: "4.108",
-                        data: {},
-                        children: []
-                    }]
-                }]
-            }, {
-                id: "node2109",
-                name: "2.109",
-                data: {},
-                children: [{
-                    id: "node3110",
-                    name: "3.110",
-                    data: {},
-                    children: [{
-                        id: "node4111",
-                        name: "4.111",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4112",
-                        name: "4.112",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node3113",
-                    name: "3.113",
-                    data: {},
-                    children: [{
-                        id: "node4114",
-                        name: "4.114",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4115",
-                        name: "4.115",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4116",
-                        name: "4.116",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node3117",
-                    name: "3.117",
-                    data: {},
-                    children: [{
-                        id: "node4118",
-                        name: "4.118",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4119",
-                        name: "4.119",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4120",
-                        name: "4.120",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4121",
-                        name: "4.121",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node3122",
-                    name: "3.122",
-                    data: {},
-                    children: [{
-                        id: "node4123",
-                        name: "4.123",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4124",
-                        name: "4.124",
-                        data: {},
-                        children: []
-                    }]
-                }]
-            }, {
-                id: "node2125",
-                name: "2.125",
-                data: {},
-                children: [{
-                    id: "node3126",
-                    name: "3.126",
-                    data: {},
-                    children: [{
-                        id: "node4127",
-                        name: "4.127",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4128",
-                        name: "4.128",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4129",
-                        name: "4.129",
-                        data: {},
-                        children: []
-                    }]
-                }]
-            }]
-        }, {
-            id: "node1130",
-            name: "1.130",
-            data: {},
-            children: [{
-                id: "node2131",
-                name: "2.131",
-                data: {},
-                children: [{
-                    id: "node3132",
-                    name: "3.132",
-                    data: {},
-                    children: [{
-                        id: "node4133",
-                        name: "4.133",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4134",
-                        name: "4.134",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4135",
-                        name: "4.135",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4136",
-                        name: "4.136",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4137",
-                        name: "4.137",
-                        data: {},
-                        children: []
-                    }]
-                }]
-            }, {
-                id: "node2138",
-                name: "2.138",
-                data: {},
-                children: [{
-                    id: "node3139",
-                    name: "3.139",
-                    data: {},
-                    children: [{
-                        id: "node4140",
-                        name: "4.140",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4141",
-                        name: "4.141",
-                        data: {},
-                        children: []
-                    }]
-                }, {
-                    id: "node3142",
-                    name: "3.142",
-                    data: {},
-                    children: [{
-                        id: "node4143",
-                        name: "4.143",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4144",
-                        name: "4.144",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4145",
-                        name: "4.145",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4146",
-                        name: "4.146",
-                        data: {},
-                        children: []
-                    }, {
-                        id: "node4147",
-                        name: "4.147",
-                        data: {},
-                        children: []
-                    }]
-                }]
-            }]
         }]
     };
     //end
@@ -780,10 +365,10 @@ function init(){
         //set overridable=true for styling individual
         //nodes or edges
         Node: {
-            height: 20,
-            width: 60,
+            height: 60,
+            width: 100,
             type: 'rectangle',
-            color: '#aaa',
+            color: '#eef0dd',
             overridable: true
         },
         
@@ -815,8 +400,8 @@ function init(){
             };
             //set label styles
             var style = label.style;
-            style.width = 60 + 'px';
-            style.height = 17 + 'px';            
+            style.width = 100 + 'px';
+            style.height = 60 + 'px';            
             style.cursor = 'pointer';
             style.color = '#333';
             style.fontSize = '0.8em';
@@ -833,7 +418,7 @@ function init(){
             //add some color to the nodes in the path between the
             //root node and the selected node.
             if (node.selected) {
-                node.data.$color = "#ff7";
+                node.data.$color = "#d8dac7";
             }
             else {
                 delete node.data.$color;
@@ -844,7 +429,7 @@ function init(){
                     node.eachSubnode(function(n) { count++; });
                     //assign a node color based on
                     //how many children it has
-                    node.data.$color = ['#aaa', '#baa', '#caa', '#daa', '#eaa', '#faa'][count];                    
+                    //node.data.$color = ['#aaa', '#baa', '#caa', '#daa', '#eaa', '#faa'][count];                    
                 }
             }
         },
